5336417374ab3f46ab79b743fc071131e6184c40
CElizOwens <owens.christina@gmail.com>
2023-04-13 10:00:02 -0700

    Created scss-notes.txt Created, consolidated more common scss classes to reset.scss Began updating, improving login and signup pages
    Co-authored-by: Christina Walton
    celizowens@users.noreply.github.com
    Co-authored-by: Giovana De La Cruz
    giovanacdlc@users.noreply.github.com
    Co-authored-by: Nicholas Ly
    nicholas-l-ly@users.noreply.github.com
    Co-authored-by: Timmy Zhu
    timzhu15@users.noreply.github.com

--- client/pages/TripHome/NewTripPage.jsx
+++ client/pages/TripHome/NewTripPage.jsx
@@ -3,109 +3,138 @@ This page hasn't been tested by me personally, but the backend functionality is 
 support the creation of a new trip given the correct information.
 */
 
-import React, { useState } from "react";
-import { redirect, Form } from "react-router-dom";
+import React, { useState } from 'react';
+import { redirect, Form } from 'react-router-dom';
+import RootLayout from '../../layouts/rootLayout';
 import '../../scss/NewTripPage.scss';
-import RootLayout from "../../layouts/rootLayout";
-import { useContext } from "react";
-import { userContext } from "../../context";
+import { useContext } from 'react';
+import { userContext } from '../../context';
 //Will have access to userId
 const newTripPage = () => {
-    const [date, setDate] = useState('');
-    const [location, setLocation] = useState('');
-    const [tripType, setTripType] = useState('');
-    const [tripName, setTripName] = useState('');
-    const { user, setUser } = useContext(userContext);
+  const [date, setDate] = useState('');
+  const [location, setLocation] = useState('');
+  const [tripType, setTripType] = useState('');
+  const [tripName, setTripName] = useState('');
+  const { user, setUser } = useContext(userContext);
 
-    const userId = user.user_id
+  const userId = user.user_id;
 
-    // handler function for the input fields
-    const handleLocation = (e) => {
-        setLocation(e.target.value);
-    }
-    const handleTripType = (e) => {
-        setTripType(e.target.value);
-    }
-    const handleDate = (e) => {
-        setDate(e.target.value);
-    }
-    const handleTripName = (e) => {
-        setTripName(e.target.value);
-    }
+  // handler function for the input fields
+  const handleLocation = (e) => {
+    setLocation(e.target.value);
+  };
+  const handleTripType = (e) => {
+    setTripType(e.target.value);
+  };
+  const handleDate = (e) => {
+    setDate(e.target.value);
+  };
+  const handleTripName = (e) => {
+    setTripName(e.target.value);
+  };
 
-    // this functioin send a post request to the data base to grab the _id of
-    // of the new trip that was created in the database and redirects the user
-    // to the tripsHome page. From there you can add items and uers, etc with put/patch requests 
+  // this functioin send a post request to the data base to grab the _id of
+  // of the new trip that was created in the database and redirects the user
+  // to the tripsHome page. From there you can add items and uers, etc with put/patch requests
 
-    // below neeeds user ID as a global variable to work
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        
-        // post request to server // need to get the userID 
+  // below neeeds user ID as a global variable to work
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        fetch(`/api/trip/create-trip/${userId}`, {
-            method: "POST",
-            body: JSON.stringify({location: location, tripType: tripType, date: date, tripName: tripName})
-        })
-        .then(res => res.json())
-        .then((res) => {
-            // this responses should have the _id of the new trips' database
-            // id --> _id
-            // reset the state of the page to force re-render
-            setLocation('');
-            setDate('');
-            setTripType('');
-            setTripName('');
-            // grab the _id from the res -> also has
-            
-            //FixURL
-            const URL = '/TripHomePage' + res.trip_id
-            
+    // post request to server // need to get the userID
+
+    fetch(`/api/trip/create-trip/${userId}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        location: location,
+        tripType: tripType,
+        date: date,
+        tripName: tripName,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        // this responses should have the _id of the new trips' database
+        // id --> _id
+        // reset the state of the page to force re-render
+        setLocation('');
+        setDate('');
+        setTripType('');
+        setTripName('');
+        // grab the _id from the res -> also has
+
+        //FixURL
+        const URL = '/TripHomePage' + res.trip_id;
+
         // invoke prop drilled setCurrentTrip, pass in trip object
 
         //     redirect to the trips home page
-            return redirect(URL);
-        })
-        .catch((err) => {
-            console.log(err);
-            alert('Failed To Create Trip');
-        }); 
-    };
+        return redirect(URL);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert('Failed To Create Trip');
+      });
+  };
 
-// <Form method={} action={} onSubmit={handleSubmit}> 
-    return (
-        // *** QUESTION: is the action leading to the correct page?
-        <div>
-            <RootLayout/>
-        <main className='new-trip-page'>
-              
-            <p className='title'>       Start Planning Your Dream Trip!     </p>
-            <div className='container'>
-                {/* on submit right here activates the button */}
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        <span className='question'>Where are you going?</span>
-                        <input className='new-trip-text' type="text" value={location} name="location" onChange={handleLocation}/>
-                    </label>
-                    <label>
-                        <span className='question'>When are you going?</span>
-                        <input className='new-trip-text' type="text" value={date} name="date" onChange={handleDate}/>
-                    </label>
-                    <label>
-                        <span className='question'>What are you planning for?</span>
-                        <input className='new-trip-text' type="text" value={tripType} name="tripType" onChange={handleTripType}/>
-                    </label>
-                    <label>
-                        <span className='question'>What will you call this Epic Adventure?</span>
-                        <input className='new-trip-text' type="text" value={tripName} name="tripName" onChange={handleTripName}/>
-                    </label>
-                    <button type="submit">Create Trip!</button>
-                </form>
-            </div>
-
-        </main>
+  // <Form method={} action={} onSubmit={handleSubmit}>
+  return (
+    // *** QUESTION: is the action leading to the correct page?
+    <div>
+      <RootLayout />
+      <main className="page new-trip-page">
+        <h1 className="all-headers title">Start Planning Your Dream Trip!</h1>
+        <div className="container">
+          {/* on submit right here activates the button */}
+          <form onSubmit={handleSubmit}>
+            <label>
+              <span className="question">Where are you going?</span>
+              <input
+                className="user-input"
+                type="text"
+                value={location}
+                name="location"
+                onChange={handleLocation}
+              />
+            </label>
+            <label>
+              <span className="question">When are you going?</span>
+              <input
+                className="user-input"
+                type="text"
+                value={date}
+                name="date"
+                onChange={handleDate}
+              />
+            </label>
+            <label>
+              <span className="question">What are you planning for?</span>
+              <input
+                className="user-input"
+                type="text"
+                value={tripType}
+                name="tripType"
+                onChange={handleTripType}
+              />
+            </label>
+            <label>
+              <span className="question">
+                What will you call this Epic Adventure?
+              </span>
+              <input
+                className="user-input"
+                type="text"
+                value={tripName}
+                name="tripName"
+                onChange={handleTripName}
+              />
+            </label>
+            <button type="submit">Create Trip!</button>
+          </form>
         </div>
-    );
+      </main>
+    </div>
+  );
 };
 
 export default newTripPage;
--- client/pages/UserHome/UserHomePage.jsx
+++ client/pages/UserHome/UserHomePage.jsx
@@ -6,49 +6,57 @@ Clicking the "Let's Go" button should save that trip to the global context "curr
 I didn't have time to test. 
 */
 
-
-
 import React, { useState, useContext, useEffect } from 'react';
 import { userContext } from '../../context';
 import { useNavigate } from 'react-router-dom';
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route,Routes } from "react-router-dom";
-import RootLayout from '../../layouts/rootLayout'
-import NewTripPage from '../TripHome/NewTripPage';
-import TripHomePage from '../TripHome/TripHomePage';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import RootLayout from '../../layouts/rootLayout';
+import '../../scss/UserHomePage.scss';
+// import NewTripPage from '../TripHome/NewTripPage';
+// import TripHomePage from '../TripHome/TripHomePage';
 import UserTripDisplay from './UserTripDisplay';
-import { NavLink, Outlet } from "react-router-dom";
+// import { NavLink, Outlet } from 'react-router-dom';
 function UserHomePage() {
   const [joinTripCode, setJoinTripCode] = useState('');
   const [tripsArray, setTripsArray] = useState([]);
   const { user, setUser } = useContext(userContext);
   const navigate = useNavigate();
 
-  
-
-     
   const handleCreateTrip = (e) => {
-      e.preventDefault();
-      return navigate('/NewTripPage');
-  }
+    e.preventDefault();
+    return navigate('/NewTripPage');
+  };
 
   // NOT built yet
   const handleJoinTrip = (e) => {
-      e.preventDefault();
-      console.log(user);
-      // patch
-      // TODO make functionality in backend lol
-      // setCurrentTrip(returned trip)
-      // return navigate('/TripHomePage');
-  }
+    e.preventDefault();
+    console.log(user);
+    // patch
+    // TODO make functionality in backend lol
+    // setCurrentTrip(returned trip)
+    // return navigate('/TripHomePage');
+  };
 
   //This function makes the array of UserTripDisplay components with the correct info passed down,
   // but only after useContext provides a real value. The first time the page loads user from useContext
   // is undefined.
   function makeTrips(userTripArray) {
     if (!userTripArray) return [];
-    return userTripArray.map(trip => {
-      return <UserTripDisplay tripName={trip.tripName} date={trip.date} trip_id={trip.trip_id} />
-    })
+    return userTripArray.map((trip) => {
+      return (
+        <UserTripDisplay
+          tripName={trip.tripName}
+          date={trip.date}
+          trip_id={trip.trip_id}
+        />
+      );
+    });
   }
 
   //Checks to see when user.trips has been updated, aka loaded by useContext, and then updates state to render the trip info
@@ -56,32 +64,35 @@ function UserHomePage() {
   //   setTripsArray(makeTrips(user.trips));
   // }, [user.trips]);
 
-
-    return (
-
-
-      
-        <div className="user-home-page"> 
-          <RootLayout/>
-          {/* <div>testing is here: {user}</div> */}
-
-            <div className='create-trip'>
-                <button onClick={handleCreateTrip}>Create New Trip</button>
-            </div>
-            <div className='join-trip'>
-                <input type="text" value={joinTripCode} onChange={(e) => setJoinTripCode(e.target.value)} />
-                <button onClick={handleJoinTrip}>Join Trip</button>
-            </div>
-            <div className='current-trips'>
-              <h2>Currently Planning</h2>
-              {tripsArray}
-            </div>
-            <div className='past-trips'>
-              <h2>Past Trips</h2>
-            </div>
+  return (
+    <div>
+      <RootLayout />
+      {/* <div>testing is here: {user}</div> */}
+      <main className="page user-home-page">
+        <h1 className="title">Home Page</h1>
+        <div className="container">
+          <div className="create-trip">
+            <button onClick={handleCreateTrip}>Create New Trip</button>
+          </div>
+          <div className="join-trip">
+            <input
+              type="text"
+              value={joinTripCode}
+              onChange={(e) => setJoinTripCode(e.target.value)}
+            />
+            <button onClick={handleJoinTrip}>Join Trip</button>
+          </div>
+          <div className="current-trips">
+            <h2>Currently Planning</h2>
+            {tripsArray}
+          </div>
+          <div className="past-trips">
+            <h2>Past Trips</h2>
+          </div>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
-
 
 export default UserHomePage;
